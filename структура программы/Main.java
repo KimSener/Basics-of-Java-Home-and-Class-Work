a3fc944f9a863c606995e98f3c913d6265ae8209
KimSener <107429314+KimSener@users.noreply.github.com>
2022-06-20 19:47:20 +0500

    Add files via upload

---
+++ структура программы/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+
+  public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     System.out.print("Введите первое число: ");
+     int value1 = scanner.nextInt();
+     System.out.print("Введите первое число: ");
+     int value2 = scanner.nextInt();
+     System.out.print("Введите первое число: ");
+     int value3 = scanner.nextInt();
+     int result = sum(value1,value2,value3);
+     System.out.println("Сумма чисел равна: " + result);
+  }
+  public static int sum(int value1, int value2, int value3) {
+     return value1 + value2 +value3;
+  }
+}

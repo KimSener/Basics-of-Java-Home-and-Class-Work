2a758e3a4e053bcc680df84e80d7e38261bc4c24
KimSener <107429314+KimSener@users.noreply.github.com>
2022-06-23 20:12:50 +0500

    Add files via upload

---
+++ введение/Main.java
@@ -0,0 +1,26 @@
+class Main {
+
+  public static void main(String[] args) {
+     int i = 5;
+     int x = 5;
+     int y = x;
+     x = x+1;
+     String text = "Fillip";
+     String myCar = "Fillip";
+     final String My_Good_Car = "Fillip";
+     String myGoofCar;
+     System.out.println("Hello World");
+     System.out.println("Hello netology");
+     System.out.println("Hello" + i + "!!");
+     System.out.println("Hello" + y + "!!");
+     System.out.println("Hello" + text + "!!");
+     System.out.println("Hello" + My_Good_Car + "!!");
+  }
+
+
+
+}
+
+
+
+

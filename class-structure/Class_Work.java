634681c1ce7de4357d6d563a910a1825db41fad7
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-05 01:33:17 +0500

    Update Class_Work.java

--- class-structure/Class_Work.java
+++ class-structure/Class_Work.java
@@ -1,1 +1,25 @@
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Test.muFanc(10));
+        f();
+        System.out.println(mainField);
+        Singer singer = new Singer();
+        singer.name = "2Pac";
+        singer.age = 25;
+        singer.rating = 5;
+        System.out.println(singer.isTooYoung());
+        singer.happyBirthday();
+        System.out.println(singer.age);
+
+        System.out.println(singer.name);
+        Singer singer2 = new Singer();
+        singer2.name = "Vasya";
+        singer2.age = 7;
+        System.out.println(singer2.isTooYoung());
+    }
+    public static void f(){
+        System.out.println("Hello, Main");
+    }
+    public static int mainField =3;
+}

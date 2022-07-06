3526cfad04f540e919f6ea81166da3824228a176
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-06 21:40:36 +0500

    Update and rename Class_Work.java to Class_Work_1.java

--- class-structure/Class_Work_1.java
+++ class-structure/Class_Work_1.java
@@ -3,19 +3,19 @@ public class Main {
         System.out.println(Test.muFanc(10));
         f();
         System.out.println(mainField);
-        Singer singer = new Singer();
+        Singer singer = new Singer("2Pac",25,5);
         singer.name = "2Pac";
         singer.age = 25;
         singer.rating = 5;
         System.out.println(singer.isTooYoung());
         singer.happyBirthday();
         System.out.println(singer.age);
-
-        System.out.println(singer.name);
-        Singer singer2 = new Singer();
+        Singer singer2 = new Singer("Vasya",7);
         singer2.name = "Vasya";
         singer2.age = 7;
         System.out.println(singer2.isTooYoung());
+        System.out.println(singer);
+
     }
     public static void f(){
         System.out.println("Hello, Main");

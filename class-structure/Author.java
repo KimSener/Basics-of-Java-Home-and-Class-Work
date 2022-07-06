9833fff08331ad1c21027f907d825b84dc528fcf
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-06 21:45:53 +0500

    Add files via upload

---
+++ class-structure/Author.java
@@ -0,0 +1,50 @@
+public class Author {
+
+    public String name;
+    public static int yearPublication;
+    public static int yearPublicationTwo;
+    public static int yearPublicationThree;
+    public String nameWriter;
+    public String nameWriterTwo;
+    public String nameWriterThree;
+    public int cost;
+
+    public String toString() {
+        return name;
+    }
+
+    public int year() {
+        return yearPublication;
+    }
+
+    public int yearTwo() {
+        return yearPublicationTwo;
+    }
+
+    public int yearThree() {
+        return yearPublicationThree;
+    }
+
+    public Author(String name, String nameWriter, int cost) {
+        this.name = name;
+        yearPublication = 2015;
+        this.nameWriter = nameWriter;
+        this.cost = cost;
+    }
+
+    public Author(String name) {
+        this.name = name;
+        yearPublicationTwo = 1987;
+        nameWriterTwo = "George Lukas";
+        this.cost = 8000;
+    }
+
+    public Author(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+        nameWriterThree = "Tim Hard";
+        yearPublicationThree = 2008;
+    }
+
+}
+

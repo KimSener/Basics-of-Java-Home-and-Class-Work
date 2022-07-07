a99791efdf6f6337c8ee61e8ed3af20d82df7202
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-07 19:39:17 +0500

    Update Home_Work_1.5.3.java

--- reference-types/Home_Work_1.5.3.java
+++ reference-types/Home_Work_1.5.3.java
@@ -16,7 +16,7 @@ public class Test {
             long value2 = Long.parseLong(scanner.next().trim());
             System.out.println("Выберите операцию abs, div, div_round, pow");
             Operation operation = Operation.valueOf(scanner.next().trim());
-            calculate(value1, value2, operation);
+            calculate(valueOne, valueTwo, operation);
         }
     }
 
@@ -27,20 +27,20 @@ public class Test {
         pow
     }
 
-    private static void calculate(long value1, long value2, Operation operation) {
+    private static void calculate(long valueOne, long valueTwo, Operation operation) {
         switch (operation) {
             case abs:
-                System.out.printf("value1 abs = %s %n", Math.abs(value1));
-                System.out.printf("value2 abs = %s %n", Math.abs(value2));
+                System.out.printf("valueOne abs = %s %n", Math.abs(valueOne));
+                System.out.printf("valueTwo abs = %s %n", Math.abs(valueTwo));
                 break;
             case div:
-                System.out.printf("div = %s %n", (double) value1 / value2);
+                System.out.printf("div = %s %n", (double) valueOne / valueTwo);
                 break;
             case div_round:
-                System.out.printf("round div = %s %n", Math.round((double) value1 / value2));
+                System.out.printf("round div = %s %n", Math.round((double) valueOne / valueTwo));
                 break;
             case pow:
-                System.out.printf("pow = %s %n", Math.pow(value1, value2));
+                System.out.printf("pow = %s %n", Math.pow(valueOne, valueTwo));
                 break;
         }
         System.out.println("-----------------------------------------------");

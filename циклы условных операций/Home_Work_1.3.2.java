1379684a2f0eb6256363e149d750dd6cd61503f3
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-08 00:12:58 +0500

    Update Home_Work_1.3.2.java

--- циклы условных операций/Home_Work_1.3.2.java
+++ циклы условных операций/Home_Work_1.3.2.java
@@ -6,12 +6,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             total++;
+            System.out.println("Введите год и количество дней в формате yyyy число");
             int year = scanner.nextInt();
+            System.out.println("Вы ввели:" + " " + year + " " + "год");
             int day = scanner.nextInt();
+            System.out.println("Вы ввели количество дней равное:" + " " + day);
             if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && day == 366 || day == 365) {
-                System.out.println("Правильно!!!");
+                System.out.println("Ответ правильный!!!");
             } else {
-                System.out.println("Неправильно!!!");
+                System.out.println("Ответ Неправильный!!!");
                 System.out.print("Набранно очков:" + " " + total);
                 break;
             }

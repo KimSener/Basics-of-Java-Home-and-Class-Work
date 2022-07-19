3967da7627085ef1e49e4672ed3797129457b9d4
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-19 21:01:46 +0500

    Update Book.java

--- inheritance/Home_Works/Home_Work_Task_3.2.1/Book.java
+++ inheritance/Home_Works/Home_Work_Task_3.2.1/Book.java
@@ -1,7 +1,6 @@
 
 public class Book {
-    private final String title;   //Т.к. массив – это объект, то final означает, что после присвоения ссылки на объект, 
-                                  //уже нельзя ее изменить, но можно изменять состояние объекта.
+    private final String title;// модификатор final создает переменную для каждого обьекта, а модификатор static final создает переменную только один раз экономяя память
     private final Genre[] genres;
 
     public Book(String title, Genre[] genres) {

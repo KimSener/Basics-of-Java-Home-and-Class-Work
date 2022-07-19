61eed0a96e18c8ce3864d7b370387380b66ae14b
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-20 00:22:11 +0500

    Add files via upload

---
+++ Polymorphism/Class_Work.java/Figure.java
@@ -0,0 +1,19 @@
+public class Figure {
+    String color;
+
+
+public Figure (String color) {
+   this.color = color;
+}
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+throw new UnsupportedOperationException();
+    }
+    public double getPerimetr() {
+        throw new UnsupportedOperationException();
+    }
+}

a9551ede6cf9ff0f167c9bf9a9e42faf709b99cc
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-20 22:06:02 +0500

    Update Book.java

--- inheritance/Home_Works/Home_Work_Task_3.2.1/Book.java
+++ inheritance/Home_Works/Home_Work_Task_3.2.1/Book.java
@@ -1,6 +1,6 @@
 
 public class Book {
-    private final String title;// модификатор final создает переменную для каждого обьекта, а модификатор static final создает переменную только один раз экономяя память
+    private final String title;// переменная константа которые нельзя изменить
     private final Genre[] genres;
 
     public Book(String title, Genre[] genres) {

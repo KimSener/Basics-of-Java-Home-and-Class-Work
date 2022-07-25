b0c41eb4824993390ce6499a4c494f297cfb2a64
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-26 01:49:10 +0500

    Create Main.java

---
+++ abstractions-interfaces/Class_Work.java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.move("Петя", "Спб", "Москву");
+        spbToMoscow("Petya", new Car());
+    }
+
+    public static void spbToMoscow(String name, Mover mover) {
+        mover.move(name, "spb", "Moscow");
+    }
+}

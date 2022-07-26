a7885c20efa6d62481901d0aa6a661097617e150
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-26 22:54:43 +0500

    Add files via upload

---
+++ abstractions-interfaces/Home_Work/Package Interfaces/task 3.3.1/Administrator.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface Administrator {
+    void overdueNotification(Reader reader);
+
+    void outBook(Reader reader);
+
+    void searchBook(Reader reader);
+
+}

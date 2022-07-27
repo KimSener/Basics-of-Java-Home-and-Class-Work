dd1d5da5a0338b378c7e6133379ab906ca550137
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-28 02:55:33 +0500

    Add files via upload

---
+++ abstractions-interfaces/Home_Work/Task_3.3.2_Bank accounts/Account.java
@@ -0,0 +1,20 @@
+public abstract class Account implements getBalance {
+    protected int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public void pay(int amount) {
+
+    }
+
+    public void transfer(Account account, int amount) {
+
+    }
+
+    public void addMoney(int amount) {
+
+    }
+
+}

e69ba001a1a8c2ff4122cd0fd56c26cd1bff6fff
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-31 23:17:22 +0500

    Add files via upload

---
+++ exceptions/Home_Work/Task_3.4.2_Validation of read events/Event.java
@@ -0,0 +1,26 @@
+package Task2;
+
+public abstract class Event {
+    private String title;
+    private int releaseYear;
+    private int age;
+
+    public Event(String title, int releaseYear, int age) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.age = age;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}

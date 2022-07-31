66faec79ef0093ff740e5ac832c72024d9b2a486
KimSener <107429314+KimSener@users.noreply.github.com>
2022-07-31 23:14:27 +0500

    Add files via upload

---
+++ exceptions/Home_Work/Task_3.4.1_Checking access to a resource/User.java
@@ -0,0 +1,26 @@
+public class User {
+    protected String login;
+    protected String email;
+    protected String password;
+    protected int age;
+
+    public User(String login, String email, String password, int age) {
+        this.login = login;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}

82c4c42a2bed0188deac7f414cc199dafd613ebd
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-07 22:37:43 +0500

    Add files via upload

---
+++ Generics/Class_Work.java/Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+    private final int id;
+
+    public Animal(int id) {
+        this.id = id;
+    }
+
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(id);
+    }
+}

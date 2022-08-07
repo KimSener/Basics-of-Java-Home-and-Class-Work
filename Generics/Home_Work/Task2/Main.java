754827805c386c3951ac4d414e26332dc0c3b60e
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-07 22:44:08 +0500

    Create Main.java

---
+++ Generics/Home_Work/Task2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // параметризируем класс типом String для ключа и значения
+        Box<String, String> sample1 = new Box<>("имя", "Нетология");
+        System.out.println(sample1);
+        // параметризируем класс типом Integer для ключа и Boolean для значения
+        Box<Integer, Boolean> sample2 = new Box<>(1, Boolean.TRUE);
+        System.out.println(sample2);
+    }
+}

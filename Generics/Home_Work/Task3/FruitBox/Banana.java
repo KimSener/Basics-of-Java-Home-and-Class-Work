044b02965a327207ee29f9546ae95c2f7bbecbb4
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-07 22:47:26 +0500

    Add files via upload

---
+++ Generics/Home_Work/Task3/FruitBox/Banana.java
@@ -0,0 +1,9 @@
+package hometask.fruitBox;
+import hometask.Fruit;
+
+public class Banana extends Fruit {
+    @Override
+    public void printClass() {
+        System.out.println("I am sub class Banana");
+    }
+}

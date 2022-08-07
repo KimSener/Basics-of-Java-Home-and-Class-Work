5e9a8f92427b745f37c2735d8312c703f1159983
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-07 22:49:16 +0500

    Add files via upload

---
+++ Generics/Home_Work/Task3/VegetableBox/Potato.java
@@ -0,0 +1,10 @@
+package hometask.vegetableBox;
+
+import hometask.Vegetable;
+
+public class Potato extends Vegetable {
+    @Override
+    public void printClass() {
+        System.out.println("I am sub class Potato");
+    }
+}

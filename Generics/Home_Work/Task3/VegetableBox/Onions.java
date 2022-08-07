a934ab5cd860e4a086066f9698076fad3c7ee428
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-07 22:48:58 +0500

    Add files via upload

---
+++ Generics/Home_Work/Task3/VegetableBox/Onions.java
@@ -0,0 +1,10 @@
+package hometask.vegetableBox;
+
+import hometask.Vegetable;
+
+public class Onions extends Vegetable {
+    @Override
+    public void printClass() {
+        System.out.println("I am sub class Onions");
+    }
+}

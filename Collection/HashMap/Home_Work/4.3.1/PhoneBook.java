dcaee64abb39411f652a1d839670407a5b6af81e
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-23 02:25:42 +0500

    Add files via upload

---
+++ Collection/HashMap/Home_Work/4.3.1/PhoneBook.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class PhoneBook {
+
+    private Map<String, List<Contact>> contact = new HashMap<>();
+
+
+    public void addGroup(String group) {
+        if (contact.containsKey(group)) {
+            System.out.println("Внимание Группа с таким именем уже существует");
+            System.out.println();
+        } else {
+            contact.put(group, new ArrayList<>());
+
+        }
+    }
+
+    public void contactsAdd(Contact contacts, String group) {
+        contact.get(group).add(contacts);
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Список всех групп :\n");
+
+        for (String group : contact.keySet()) {
+            stringBuilder.append("\t").append(group).append(" \n");
+            List<Contact> contacts = contact.get(group);
+            if (contacts.isEmpty()) {
+                System.out.println("Группа контактов" + " " + group + " " + "пуста!");
+            } else {
+                for (Contact contact : contacts) {
+                    stringBuilder.append("\t\t\t").append(contact).append("\n");
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public Contact findContact(String phoneNumber) {
+        for (List<Contact> contacts : contact.values()) {
+            for (Contact contactOne : contacts) {
+                if (contactOne.getPhoneNumber().equals(phoneNumber)) {
+                    return contactOne;
+                }
+            }
+
+
+        }
+        return null;
+    }
+
+
+}
+

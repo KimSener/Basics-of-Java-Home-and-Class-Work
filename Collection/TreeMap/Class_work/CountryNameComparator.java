274a41020efa3c80252ff9779cdea67a22148648
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-23 02:49:29 +0500

    Add files via upload

---
+++ Collection/TreeMap/Class_work/CountryNameComparator.java
@@ -0,0 +1,10 @@
+package ClassWork;
+
+import java.util.Comparator;
+
+public class CountryNameComparator implements Comparator<Country> {
+    @Override
+    public int compare(Country c1, Country c2) {
+        return c1.name.compareTo(c2.name);
+    }
+}

42420e54575186e9ae2cd76c19bd17ecd7d72afe
KimSener <107429314+KimSener@users.noreply.github.com>
2022-08-23 02:50:17 +0500

    Add files via upload

---
+++ Collection/TreeMap/Class_work/Country.java
@@ -0,0 +1,34 @@
+package ClassWork;
+
+
+public class Country implements Comparable<Country> {
+    protected String name;
+    protected int population;
+
+    public Country(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    @Override
+    public int compareTo(Country country) {
+        if (population > country.population) {
+            return 1;
+        } else if (population < country.population) {
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+       return ((Country)obj).population == population;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + population + ") ";
+
+    }
+}
